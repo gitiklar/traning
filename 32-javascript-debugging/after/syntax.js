f9888e56c25a65c3c7fc77be40306ed4a63fa92f
gita <gita@date4dos.co.il>
2022-11-09 00:01:58 +0200

    lesson 32

--- 32-javascript-debugging/after/syntax.js
+++ 32-javascript-debugging/after/syntax.js
@@ -2,7 +2,7 @@
 // fact(4) = 1 * 2 * 3 * 4 = 24
 function fact(n) {
   let result = 1;
-  for (let i=1; i <= n; i++) {
+  for (let i = 1; i <= n; i++) {
     result *= i;
   }
   return result;
@@ -12,7 +12,7 @@ console.log(fact(4));
 
 let i = 99;
 
-while ( i > 0 ) {
+while (i > 0) {
   console.log(`${i} bottles of beer on the wall`);
   i--;
 }
