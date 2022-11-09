1ce97d65331a12b3c88bede158b776201d635eed
gita <gita@date4dos.co.il>
2022-11-09 18:29:28 +0200

    lesson 43

--- 43-timers/before/intervals_demo.js
+++ 43-timers/before/intervals_demo.js
@@ -1,3 +1,25 @@
-const startIntervalDemo = document.querySelector('#start-interval-demo');
-const cancelIntervalDemo = document.querySelector('#cancel-interval-demo');
-const result2 = document.querySelector('#result2');
+(() => {
+  const startIntervalDemo = document.querySelector("#start-interval-demo");
+  const cancelIntervalDemo = document.querySelector("#cancel-interval-demo");
+  const result2 = document.querySelector("#result2");
+
+  const texts = ["Wait for it...", "Hi!", "bye", ""];
+
+  let num = 0;
+  const tick = () => {
+    result2.textContent = texts[num % texts.length];
+    num++;
+  };
+
+  let idTimeout;
+
+  startIntervalDemo.addEventListener("click", () => {
+    idTimeout && clearInterval(idTimeout);
+
+    idTimeout = setInterval(tick, 1000);
+  });
+
+  cancelIntervalDemo.addEventListener("click", () => {
+    clearInterval(idTimeout);
+  });
+})();
--- 43-timers/before/timers_demo.js
+++ 43-timers/before/timers_demo.js
@@ -1,3 +1,20 @@
-const startTimeoutDemo = document.querySelector('#start-timeout-demo');
-const cancelTimeoutDemo = document.querySelector('#cancel-timeout-demo');
-const result1 = document.querySelector('#result1');
+const startTimeoutDemo = document.querySelector("#start-timeout-demo");
+const cancelTimeoutDemo = document.querySelector("#cancel-timeout-demo");
+const result1 = document.querySelector("#result1");
+const texts = ["Wait for it...", "Hi!", "bye", ""];
+
+let num = 0;
+const tick = () => {
+  result1.textContent = texts[num % texts.length];
+  num++;
+};
+
+let idTimeout;
+
+startTimeoutDemo.addEventListener("click", () => {
+  idTimeout = setTimeout(tick, 1000);
+});
+
+cancelTimeoutDemo.addEventListener("click", () => {
+  clearTimeout(idTimeout);
+});
