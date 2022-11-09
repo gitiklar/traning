571414b36f83188732cb25a2068d012256ac3752
gita <gita@date4dos.co.il>
2022-11-09 17:28:04 +0200

    lesson 38

--- 38-javascript-dom/after/table.js
+++ 38-javascript-dom/after/table.js
@@ -1,17 +1,10 @@
 let res = "";
-
-for (let i=1; i < 10; i++) {
-    res += '<tr>';
-    for (let j=1 ; j < 10; j++) {
-        res += `<td>${i * j}</td>`
-    } 
-    res += '</tr>';
+for (i = 1; i < 10; i++) {
+  res += "<tr>";
+  for (j = 1; j < 10; j++) {
+    res += `<td>${i * j}</td>`;
+  }
+  res += "</tr>";
 }
-
-console.log(res);
-
-const tbody = document.querySelector('tbody');
+const tbody = document.querySelector("tbody");
 tbody.innerHTML = res;
-
-// background-color
-document.body.style.backgroundColor = 'lightyellow';
