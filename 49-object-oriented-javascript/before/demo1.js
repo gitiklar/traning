6fecd893906a37f27a69cc5de3564f90b959a0e5
gita <gita@date4dos.co.il>
2022-11-10 12:13:46 +0200

    lesson 49

--- 49-object-oriented-javascript/before/demo1.js
+++ 49-object-oriented-javascript/before/demo1.js
@@ -1,24 +1,27 @@
-const game = {
-    secret: 0,
-    numberOfTries: 10,
-
-    startNewGame() {
-        this.secret= Math.floor(Math.random() * 10);
-        this.numberOfTries = 0;
-    },
-
-    play(guess) {
-        if (this.numberOfTries >= 10) {
-            // Sorry, game over
-            return;
-        }
-        this.numberOfTries += 1;
-        if (guess > this.secret) {
-            return 1;
-        } else if (guess < this.secret) {
-            return -1;
-        } else {
-            return 0;
-        }
-    }
+function Game() {
+  this.secret = 0;
+  this.numberOfTries = 0;
+  this.__proto__ = Game.fns;
 }
+
+Game.fns = {};
+
+Game.fns.startNewGame = function () {
+  this.secret = Math.floor(Math.random() * 10);
+  this.numberOfTries = 0;
+};
+
+Game.fns.play = function (guess) {
+  if (this.numberOfTries >= 10) {
+    alert("Sorry, game over");
+    return;
+  }
+  this.numberOfTries += 1;
+  if (guess > this.secret) {
+    return 1;
+  } else if (guess < this.secret) {
+    return -1;
+  } else {
+    return 0;
+  }
+};
