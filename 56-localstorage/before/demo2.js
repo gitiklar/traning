9bf59d669f6c32dc79bc341af737041d7c64ba03
gita <gita@date4dos.co.il>
2022-11-12 21:09:04 +0200

    lesson 56

--- 56-localstorage/before/demo2.js
+++ 56-localstorage/before/demo2.js
@@ -1,20 +1,25 @@
-const form = document.querySelector('form');
+const form = document.querySelector("form");
 
-function writeDataToForm(data) {    
-    let inp;
-    const names = Object.keys(data);
-    for (let name of names) {
-        inp = form.querySelector(`[name="${name}"]`);
-        inp.value = data[name];
-    }
+writeDataToForm(JSON.parse(localStorage.getItem("formData")) || {});
+
+function writeDataToForm(data) {
+  let inp;
+  const names = Object.keys(data);
+  for (let name of names) {
+    inp = form.querySelector(`[name="${name}"]`);
+    inp.value = data[name];
+  }
 }
 
 function readFormFieldsAsObject() {
-    const data = {};
-    const inputs = form.querySelectorAll('input,select');
-    for (let inp of inputs) {
-        data[inp.name] = inp.value;
-    }
-    return data;
+  const data = {};
+  const inputs = form.querySelectorAll("input,select");
+  for (let inp of inputs) {
+    data[inp.name] = inp.value;
+  }
+  return data;
 }
 
+form.addEventListener("input", () => {
+  localStorage.setItem("formData", JSON.stringify(readFormFieldsAsObject()));
+});
