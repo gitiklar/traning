e9a32d72314cf5cafee9469299fe12a51b5ef2c3
gita <gita@date4dos.co.il>
2022-11-14 19:06:51 +0200

    react

--- reactwith-create-react-app/src/App.js
+++ reactwith-create-react-app/src/App.js
@@ -3,12 +3,13 @@ import "./App.scss";
 import Person from "./06-props/person";
 import Counter from "./07/counter";
 import Colors from "./07/colors";
+import { MyApp } from "./12/main";
 
 function App() {
 
   return (
     <div>
-      <Counter />
+      <MyApp />
     </div>
   );
 }
