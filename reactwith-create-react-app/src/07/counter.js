1e120812e5ffccd487bd35dc670d191bfa62f7a9
gita <gita@date4dos.co.il>
2022-11-14 18:02:41 +0200

    react

--- reactwith-create-react-app/src/07/counter.js
+++ reactwith-create-react-app/src/07/counter.js
@@ -1,28 +1,42 @@
 import React from "react";
 import { useState } from "react";
+const tinycolor = require("tinycolor2");
 
 export default function Counter(props) {
-  const [delta, setDelta] = useState(1);
-  const [count, setCount] = useState(delta);
+  const [value, setValue] = useState("#ff0000");
 
-  const onClick = () => {
-    setCount((c) => c + delta);
-  };
-
-  const onInput = (e) => {
-    setDelta(Number(e.target.value));
+  const onChange = (e) => {
+    setValue(e.target.value);
   };
 
   return (
     <div>
-      <label>
-        Increase By:
-        <input type="number" onInput={onInput} value={delta} />
-      </label>
-      <p>
-        I was clicked {count} times
-        <button onClick={onClick}>Click Me</button>
-      </p>
+      <div>
+        <input type="color" value={value} onChange={onChange} />
+      </div>
+      <div style={{ display: "flex" }}>
+        {new Array(10).fill().map((_, i) => (
+          <div
+            style={{
+              width: "50px",
+              height: "50px",
+              backgroundColor:
+                i === 4
+                  ? value
+                  : i < 4
+                  ? tinycolor(value)
+                      .lighten(50 - (10 * (i + 1)))
+                      .toString()
+                  : tinycolor(value)
+                      .darken( 10*(i-5 + 1))
+                      .toString(),
+            }}
+            key={i}
+          >
+            {i}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
