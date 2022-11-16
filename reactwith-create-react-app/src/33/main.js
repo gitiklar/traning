4372162bd6bd59290e8b0b625049a3a3f45dda29
gita <gita@date4dos.co.il>
2022-11-16 12:28:10 +0200

    react

---
+++ reactwith-create-react-app/src/33/main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { MyForm, SelectHobbies, SummaryPage, UserDetails } from "./pages";
+
+export const MyApp = () => {
+  return (
+    <div>
+      <MyForm>
+        jjj
+        <SelectHobbies />
+        <UserDetails />
+        <SummaryPage />
+      </MyForm>
+    </div>
+  );
+};
