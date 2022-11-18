18dea15ef4a1c0dec9e54090a2e41c09b6bb863c
gita <gita@date4dos.co.il>
2022-11-18 14:40:49 +0200

    react

--- react2020-course-examples-master/new-react-router/47-react-router/before/src/App.js
+++ react2020-course-examples-master/new-react-router/47-react-router/before/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <h1>Hello world</h1>
+
+      <h2>Links with a</h2>
+      <a href="/home">Home </a>
+      <a href="/about">About </a>
+      <a href="Contact">Contact </a>
+
+      <h2>Links with Link</h2>
+
+      <Link to="/home">Home </Link>
+      <Link to="/about">About </Link>
+      <Link to="Contact">Contact </Link>
+
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+const Home = () => {
+  return <div>Home page</div>;
+};
+
+const About = () => {
+  return <div>About page</div>;
+};
+
+const Contact = () => {
+  return <div>Contact page</div>;
+};
 
 export default App;
