34ce4441a60ea7a5c7849c95ed608e3ade9a756d
gita <gita@date4dos.co.il>
2022-11-19 18:57:59 +0200

    react

--- react2020-course-examples-master/new-react-router/48-layout/before/src/App.js
+++ react2020-course-examples-master/new-react-router/48-layout/before/src/App.js
@@ -1,10 +1,6 @@
-import './App.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-} from "react-router-dom";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { NewTask, TasksPage, ViewTask } from "./Tasks";
 
 // tasks      => Show only a list of tasks
 // tasks/new  => Show a list of tasks on the left side,
@@ -12,7 +8,6 @@ import {
 //
 // tasks/4    => Show a list of tasks on the left side
 //               and the details of task #4 on the right
-
 
 function App() {
   return (
@@ -23,28 +18,32 @@ function App() {
         <Link to="/">Home Page</Link>
         <Link to="/about">About Page</Link>
         <Link to="/contact">Contact Page</Link>
+        <Link to="/tasks">Tasks Page</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/tasks" element={<TasksPage />}>
+          <Route index element={<p>My My My</p>} />
+          <Route path="new" element={<NewTask />} />
+          <Route path=":id" element={<ViewTask />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
 function Home() {
-  return (<p>Home Page</p>);
+  return <p>Home Page</p>;
 }
 
 function About() {
-  return (<p>About Page</p>);
+  return <p>About Page</p>;
 }
 
 function Contact() {
-  return (<p>Contact Page</p>);
+  return <p>Contact Page</p>;
 }
-
-
 
 export default App;
